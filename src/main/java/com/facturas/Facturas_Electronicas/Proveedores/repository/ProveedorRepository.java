fcd3d72a50b89e8f0e4a5210828b410f68f9fc5f
Charly <carlosgarita3110@gmail.com>
2024-03-19 16:37:13 -0600

    feat: add ProveedorRepository interface

---
+++ src/main/java/com/facturas/Facturas_Electronicas/Proveedores/repository/ProveedorRepository.java
@@ -0,0 +1,9 @@
+package com.facturas.Facturas_Electronicas.Proveedores.repository;
+
+import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProveedorRepository extends JpaRepository<ProveedorEntity, Integer> {
+}
