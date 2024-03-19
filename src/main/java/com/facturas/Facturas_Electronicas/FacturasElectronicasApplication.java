026040945af9ceaca4e01034b7f70b96e21d7280
Charly <carlosgarita3110@gmail.com>
2024-03-18 20:36:20 -0600

    feat: add initial project files

---
+++ src/main/java/com/facturas/Facturas_Electronicas/FacturasElectronicasApplication.java
@@ -0,0 +1,13 @@
+package com.facturas.Facturas_Electronicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturasElectronicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturasElectronicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facturas/Facturas_Electronicas/FacturasElectronicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturas.Facturas_Electronicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturasElectronicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
