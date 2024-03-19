c9f45e63f533bab54a2a26c162f936c1c31c8a55
Charly <carlosgarita3110@gmail.com>
2024-03-19 16:50:34 -0600

    feat: add ProveedorRepository dependency to ProveedorService constructor

--- src/main/java/com/facturas/Facturas_Electronicas/Proveedores/service/ProveedorService.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Proveedores/service/ProveedorService.java
@@ -1,8 +1,18 @@
 package com.facturas.Facturas_Electronicas.Proveedores.service;
 
+import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
+import com.facturas.Facturas_Electronicas.Proveedores.repository.ProveedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProveedorService {
 
+    private final ProveedorRepository proveedorRepository;
+
+    public ProveedorService(ProveedorRepository proveedorRepository) {
+        this.proveedorRepository = proveedorRepository;
+    }
 }
+
