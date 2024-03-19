bfb605cd91e9f4620e516d45317d228c1709c147
Charly <carlosgarita3110@gmail.com>
2024-03-19 16:37:22 -0600

    feat: add method to find ProveedorEntity by correo and contrasena

--- src/main/java/com/facturas/Facturas_Electronicas/Proveedores/repository/ProveedorRepository.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Proveedores/repository/ProveedorRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProveedorRepository extends JpaRepository<ProveedorEntity, Integer> {
+    Optional<ProveedorEntity> findProveedorEntityByCorreoAndContrasena(String correo, String contrasena);
 }
