d9c0cb6c366e2adc343b97d842a460e6a20880be
Charly <carlosgarita3110@gmail.com>
2024-03-19 17:25:11 -0600

    feat: add register page endpoint to ProveedorController

--- src/main/java/com/facturas/Facturas_Electronicas/Proveedores/controller/ProveedorController.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Proveedores/controller/ProveedorController.java
@@ -1,7 +1,12 @@
 package com.facturas.Facturas_Electronicas.Proveedores.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProveedorController {
+    @GetMapping("/register")
+    public String getRegisterProveedorPage() {
+        return "register";
+    }
 }
