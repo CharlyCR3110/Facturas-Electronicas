1ebf78a47db105170e02b18f4f41a1d2bba18980
Charly <carlosgarita3110@gmail.com>
2024-03-19 22:17:18 -0600

    refactor: update ProveedorController routes

--- src/main/java/com/facturas/Facturas_Electronicas/Proveedores/controller/ProveedorController.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Proveedores/controller/ProveedorController.java
@@ -1,17 +1,22 @@
 package com.facturas.Facturas_Electronicas.Proveedores.controller;
 
+import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
+import com.facturas.Facturas_Electronicas.Proveedores.service.ProveedorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProveedorController {
     @GetMapping("/register")
     public String getRegisterProveedorPage() {
-        return "register";
+        return "proveedor_auth/register";
     }
 
     @GetMapping("/login")
     public String getLoginProveedorPage() {
-        return "login";
+        return "proveedor_auth/login";
     }
 }
