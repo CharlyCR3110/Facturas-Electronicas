e224825e0c1c49854afc7b347ebb0283d2ba9983
Charly <carlosgarita3110@gmail.com>
2024-03-23 15:18:57 -0600

    feat: add method to retrieve products by supplier ID

--- src/main/java/com/facturas/Facturas_Electronicas/Productos/repository/ProductoRepository.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Productos/repository/ProductoRepository.java
@@ -3,6 +3,9 @@ package com.facturas.Facturas_Electronicas.Productos.repository;
 import com.facturas.Facturas_Electronicas.Productos.model.ProductoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductoRepository  extends JpaRepository<ProductoEntity, Integer> {
+import java.util.ArrayList;
 
+public interface ProductoRepository  extends JpaRepository<ProductoEntity, Integer> {
+    // metoodo para obtener la lista de productos de un proveedor
+    ArrayList<ProductoEntity> findAllByIdProveedor(int idProveedor);
 }
