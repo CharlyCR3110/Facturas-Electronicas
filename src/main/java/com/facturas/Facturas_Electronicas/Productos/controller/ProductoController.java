f851903633e75462f78fb8ac4c8f1d5ccf98920f
Charly <carlosgarita3110@gmail.com>
2024-03-23 13:45:15 -0600

    feat: add constructor to ProductoController

--- src/main/java/com/facturas/Facturas_Electronicas/Productos/controller/ProductoController.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Productos/controller/ProductoController.java
@@ -1,7 +1,13 @@
 package com.facturas.Facturas_Electronicas.Productos.controller;
 
+import com.facturas.Facturas_Electronicas.Productos.service.ProductoService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ProductoController {
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
 }
