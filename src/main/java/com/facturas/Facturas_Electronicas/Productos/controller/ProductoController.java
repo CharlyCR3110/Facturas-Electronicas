2a71c361944f471832f6c6a1fd415a6ad98aa221
Charly <carlosgarita3110@gmail.com>
2024-03-23 15:42:04 -0600

    feat: add getProductsPage method to ProductoController

--- src/main/java/com/facturas/Facturas_Electronicas/Productos/controller/ProductoController.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Productos/controller/ProductoController.java
@@ -1,12 +1,25 @@
 package com.facturas.Facturas_Electronicas.Productos.controller;
 
+import com.facturas.Facturas_Electronicas.Productos.model.ProductoEntity;
 import com.facturas.Facturas_Electronicas.Productos.service.ProductoService;
+import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 @Controller
+@SessionAttributes({"userLogged", "currentPage", "currentProduct", "currentProductList"})
 public class ProductoController {
+    // definir los atributos que se van a compartir entre los metodos del controlador
+    @ModelAttribute("currentProduct") public ProductoEntity currentProduct() { return new ProductoEntity(); }
+    @ModelAttribute("currentProductList") public ArrayList<ProductoEntity> currentProductList() { return new ArrayList<>(); }
     private final ProductoService productoService;
 
     @Autowired
@@ -14,5 +27,25 @@ public class ProductoController {
 
     public ProductoController(ProductoService productoService) {
         this.productoService = productoService;
+    }
+
+    // Metodos para manejar las peticiones GET y POST
+    @GetMapping("/products")
+    public String getProductsPage(Model model) {
+        // obtener el usuario loggeado (se obtiene de la sesion)
+        ProveedorEntity userLogged = (ProveedorEntity) httpSession.getAttribute("userLogged");
+        if (userLogged == null) {
+            return "redirect:/login";
+        }
+        // obtener la lista de productos del proveedor loggeado
+        ArrayList<ProductoEntity> productos = productoService.getProductosByProveedor(userLogged);
+
+        if (productos != null) {
+            model.addAttribute("currentProductList", productos);  // agregar al model la lista de productos
+        }
+
+        // agregar al model un identificador de la pagina actual (para el navbar)
+        model.addAttribute("currentPage", "products");
+        return "products/products";   // devuelve el view de products (templates/productos/products.html)
     }
 }
