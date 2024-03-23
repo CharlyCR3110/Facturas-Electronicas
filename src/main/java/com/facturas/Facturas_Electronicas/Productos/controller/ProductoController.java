793af590e6aac8bd067aed4eb77fc3e859dbe2a5
Charly <carlosgarita3110@gmail.com>
2024-03-23 13:46:24 -0600

    feat: add HttpSession dependency injection to ProductoController

--- src/main/java/com/facturas/Facturas_Electronicas/Productos/controller/ProductoController.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Productos/controller/ProductoController.java
@@ -1,11 +1,16 @@
 package com.facturas.Facturas_Electronicas.Productos.controller;
 
 import com.facturas.Facturas_Electronicas.Productos.service.ProductoService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ProductoController {
     private final ProductoService productoService;
+
+    @Autowired
+    private HttpSession httpSession;
 
     public ProductoController(ProductoService productoService) {
         this.productoService = productoService;
