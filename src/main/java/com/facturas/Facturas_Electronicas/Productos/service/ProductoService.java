f5f912a54bd471856edb8f63356a0cf6234ac501
Charly <carlosgarita3110@gmail.com>
2024-03-23 15:18:51 -0600

    feat: add getProductosByProveedor method to ProductoService

--- src/main/java/com/facturas/Facturas_Electronicas/Productos/service/ProductoService.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Productos/service/ProductoService.java
@@ -1,7 +1,11 @@
 package com.facturas.Facturas_Electronicas.Productos.service;
 
+import com.facturas.Facturas_Electronicas.Productos.model.ProductoEntity;
 import com.facturas.Facturas_Electronicas.Productos.repository.ProductoRepository;
+import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class ProductoService {
@@ -9,5 +13,9 @@ public class ProductoService {
 
     public ProductoService(ProductoRepository productoRepository) {
         this.productoRepository = productoRepository;
+    }
+
+    public ArrayList<ProductoEntity> getProductosByProveedor(ProveedorEntity userLogged) {
+        return productoRepository.findAllByIdProveedor(userLogged.getIdProveedor());
     }
 }
