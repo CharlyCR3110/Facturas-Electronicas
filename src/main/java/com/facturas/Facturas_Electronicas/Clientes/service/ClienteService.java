3174627ef8fa224951749d48da7d07039149c4f9
Charly <carlosgarita3110@gmail.com>
2024-03-24 10:36:50 -0600

    feat: add method to retrieve clients by provider

--- src/main/java/com/facturas/Facturas_Electronicas/Clientes/service/ClienteService.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Clientes/service/ClienteService.java
@@ -1,8 +1,12 @@
 package com.facturas.Facturas_Electronicas.Clientes.service;
 
+import com.facturas.Facturas_Electronicas.Clientes.model.ClienteEntity;
 import com.facturas.Facturas_Electronicas.Clientes.repository.ClienteRepository;
+import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class ClienteService {
@@ -10,5 +14,9 @@ public class ClienteService {
 
     public ClienteService(ClienteRepository clienteRepository) {
         this.clienteRepository = clienteRepository;
+    }
+
+    public ArrayList<ClienteEntity> getClientesByProveedor(ProveedorEntity userLogged) {
+        return clienteRepository.findAllByIdProveedor(userLogged.getIdProveedor());
     }
 }
