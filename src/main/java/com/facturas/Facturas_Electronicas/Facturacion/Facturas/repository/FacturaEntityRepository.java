0cf92580bf0b1169699dd0cae08fd6626ca58b30
Charly <carlosgarita3110@gmail.com>
2024-03-24 15:33:02 -0600

    feat: add method to retrieve invoices by supplier ID

--- src/main/java/com/facturas/Facturas_Electronicas/Facturacion/Facturas/repository/FacturaEntityRepository.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Facturacion/Facturas/repository/FacturaEntityRepository.java
@@ -2,9 +2,11 @@ package com.facturas.Facturas_Electronicas.Facturacion.Facturas.repository;
 
 
 import com.facturas.Facturas_Electronicas.Facturacion.Facturas.model.FacturaEntity;
+import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
 
 public interface FacturaEntityRepository extends JpaRepository<FacturaEntity, Integer> {
+    ArrayList<FacturaEntity> getFacturasByIdProveedor(int idProveedor);
 }
