6787fb1c66eb108507eeb78c679db5c61df50d79
Charly <carlosgarita3110@gmail.com>
2024-03-24 13:21:28 -0600

    chore: remove unused import statement and attributes in ErrorsController.java

--- src/main/java/com/facturas/Facturas_Electronicas/globals/ErrorsController.java
+++ src/main/java/com/facturas/Facturas_Electronicas/globals/ErrorsController.java
@@ -1,24 +1,11 @@
 package com.facturas.Facturas_Electronicas.globals;
 
-import com.facturas.Facturas_Electronicas.Proveedores.service.ProveedorService;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorsController implements ErrorController {
-    private final ProveedorService proveedorService;
-
-    @Autowired
-    private HttpSession httpSession;
-
-    // Constructor para "inyectar" el servicio (se usa en lugar de @Autowired)
-    public ErrorsController(ProveedorService proveedorService) {
-        this.proveedorService = proveedorService;
-    }
 
     @GetMapping("/error")
     public String getErrorPage() {
