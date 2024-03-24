02744c5c78ae223eacd1feee0848e43505895c9e
Charly <carlosgarita3110@gmail.com>
2024-03-24 10:36:43 -0600

    feat: add findAllByIdProveedor method to ClienteRepository

--- src/main/java/com/facturas/Facturas_Electronicas/Clientes/repository/ClienteRepository.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Clientes/repository/ClienteRepository.java
@@ -3,5 +3,8 @@ package com.facturas.Facturas_Electronicas.Clientes.repository;
 import com.facturas.Facturas_Electronicas.Clientes.model.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
+
 public interface ClienteRepository extends JpaRepository<ClienteEntity, Integer> {
+    ArrayList<ClienteEntity> findAllByIdProveedor(int idProveedor);
 }
