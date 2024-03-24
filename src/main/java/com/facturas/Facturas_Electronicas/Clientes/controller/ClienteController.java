4032145ad1caf003077f57a0f589a5f9502ec03b
Charly <carlosgarita3110@gmail.com>
2024-03-24 10:38:46 -0600

    feat: add getClientesPage method to ClienteController

--- src/main/java/com/facturas/Facturas_Electronicas/Clientes/controller/ClienteController.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Clientes/controller/ClienteController.java
@@ -1,12 +1,23 @@
 package com.facturas.Facturas_Electronicas.Clientes.controller;
 
+import com.facturas.Facturas_Electronicas.Clientes.model.ClienteEntity;
 import com.facturas.Facturas_Electronicas.Clientes.service.ClienteService;
+import com.facturas.Facturas_Electronicas.Productos.model.ProductoEntity;
+import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.ArrayList;
 
 @Controller
+@SessionAttributes({"userLogged", "currentPage", "currentClient", "currentClientList"})
 public class ClienteController {
+    @ModelAttribute("currentClient") public ProductoEntity currentProduct() { return new ProductoEntity(); }
     private final ClienteService clienteService;
 
     @Autowired
@@ -15,4 +26,32 @@ public class ClienteController {
     public ClienteController(ClienteService clienteService) {
         this.clienteService = clienteService;
     }
+
+    @GetMapping("/clients")
+    public String getClientesPage(Model model) {
+        // obtener el usuario loggeado (se obtiene de la sesion)
+        ProveedorEntity userLogged = (ProveedorEntity) httpSession.getAttribute("userLogged");
+        if (userLogged == null) {
+            return "redirect:/login";
+        }
+
+        if (model.getAttribute("currentClientList") == null) {
+            // obtener la lista de clientes del proveedor loggeado
+            ArrayList<ClienteEntity> clientes = clienteService.getClientesByProveedor(userLogged);
+            if (clientes != null) {
+                model.addAttribute("currentClientList", clientes);  // agregar al model la lista de clientes
+            }
+        }
+
+        ArrayList<ClienteEntity> clientes = ArrayList.class.cast(model.getAttribute("currentClientList"));
+        model.addAttribute("currentClientList", clientes);  // agregar al model la lista de clientes
+
+        model.addAttribute("errorMessage", httpSession.getAttribute("errorMessage"));
+        // eliminar el error de la sesión
+        httpSession.removeAttribute("errorMessage");
+
+        model.addAttribute("currentPage", "clients");
+        return "clients/clients";   // devuelve el view de clientes (templates/clientes/clientes.html)
+    }
+
 }
