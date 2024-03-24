006ee859678dc3d22f0f9fd0235a7ebfe2318531
Charly <carlosgarita3110@gmail.com>
2024-03-24 15:33:18 -0600

    feat: add method to retrieve invoices with details by supplier

--- src/main/java/com/facturas/Facturas_Electronicas/Facturacion/Facturas/service/FacturaEntityService.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Facturacion/Facturas/service/FacturaEntityService.java
@@ -1,13 +1,39 @@
 package com.facturas.Facturas_Electronicas.Facturacion.Facturas.service;
 
+import com.facturas.Facturas_Electronicas.Facturacion.DTO.FacturaConDetallesDTO;
+import com.facturas.Facturas_Electronicas.Facturacion.Detalles.model.DetalleFacturaEntity;
+import com.facturas.Facturas_Electronicas.Facturacion.Detalles.repository.DetalleFacturaEntityRepository;
+import com.facturas.Facturas_Electronicas.Facturacion.Facturas.model.FacturaEntity;
 import com.facturas.Facturas_Electronicas.Facturacion.Facturas.repository.FacturaEntityRepository;
+import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FacturaEntityService {
     private final FacturaEntityRepository facturaEntityRepository;
+    private final DetalleFacturaEntityRepository detalleFacturaEntityRepository;
 
-    public FacturaEntityService(FacturaEntityRepository facturaEntityRepository) {
+    public FacturaEntityService(FacturaEntityRepository facturaEntityRepository, DetalleFacturaEntityRepository detalleFacturaEntityRepository) {
         this.facturaEntityRepository = facturaEntityRepository;
+        this.detalleFacturaEntityRepository = detalleFacturaEntityRepository;
     }
+
+    public ArrayList<FacturaConDetallesDTO> getFacturasByProveedor(ProveedorEntity userLogged) {
+        ArrayList<FacturaConDetallesDTO> facturas = new ArrayList<>();
+        List<FacturaEntity> facturasDelProveedor = facturaEntityRepository.getFacturasByIdProveedor(userLogged.getIdProveedor());
+
+        for (FacturaEntity factura : facturasDelProveedor) {
+            List<DetalleFacturaEntity> detalles = detalleFacturaEntityRepository.getDetallesByIdFactura(factura.getIdFactura());
+            FacturaConDetallesDTO facturaConDetallesDTO = new FacturaConDetallesDTO();
+            facturaConDetallesDTO.setFactura(factura);
+            facturaConDetallesDTO.setDetalles(detalles);
+            facturas.add(facturaConDetallesDTO);
+        }
+
+        return facturas;
+    }
+
 }
