e7ae396b077a36fd645b7777d5a5c7cb5c77870a
Charly <carlosgarita3110@gmail.com>
2024-03-24 18:10:20 -0600

    feat: add new method to retrieve invoices by provider and client ID

--- src/main/java/com/facturas/Facturas_Electronicas/Facturacion/Facturas/repository/FacturaEntityRepository.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Facturacion/Facturas/repository/FacturaEntityRepository.java
@@ -6,7 +6,9 @@ import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface FacturaEntityRepository extends JpaRepository<FacturaEntity, Integer> {
     ArrayList<FacturaEntity> getFacturasByIdProveedor(int idProveedor);
+    List<FacturaEntity> getFacturasByIdProveedorAndIdCliente(Integer idProveedor, Integer searchClientID);
 }
