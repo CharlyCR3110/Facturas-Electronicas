4633b1ea2d2aad58f1bac5068c1fede656acf158
Charly <carlosgarita3110@gmail.com>
2024-03-29 12:30:19 -0600

    feat: add ProveedorEntity and ProveedorService imports and initialize currentProvider and currentProviderList attributes

--- src/main/java/com/facturas/Facturas_Electronicas/Admins/controller/AdminController.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Admins/controller/AdminController.java
@@ -1,20 +1,34 @@
 package com.facturas.Facturas_Electronicas.Admins.controller;
 
+import com.facturas.Facturas_Electronicas.Admins.model.AdminEntity;
 import com.facturas.Facturas_Electronicas.Admins.service.AdminService;
+import com.facturas.Facturas_Electronicas.Proveedores.model.ProveedorEntity;
+import com.facturas.Facturas_Electronicas.Proveedores.service.ProveedorService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.ArrayList;
 
 @Controller
 @SessionAttributes({"currentProvider", "currentProviderList"})
 public class AdminController {
+    @ModelAttribute("currentProvider") public ProveedorEntity currentProvider() { return new ProveedorEntity(); }
+    @ModelAttribute("currentProviderList") public ArrayList<ProveedorEntity> currentProviderList() { return new ArrayList<>(); }
+
+
     private final AdminService adminService;
+    private final ProveedorService proveedorService;
 
     @Autowired
     HttpSession httpSession;
-    
-    public AdminController(AdminService adminService) {
+
+    public AdminController(AdminService adminService, ProveedorService proveedorService) {
         this.adminService = adminService;
+        this.proveedorService = proveedorService;
     }
 }
