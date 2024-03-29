04359075f515449e745914e69d6b799f31cdc1e5
Charly <carlosgarita3110@gmail.com>
2024-03-29 11:45:46 -0600

    feat: add AdminRepository dependency to AdminService constructor

--- src/main/java/com/facturas/Facturas_Electronicas/Admins/service/AdminService.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Admins/service/AdminService.java
@@ -1,7 +1,12 @@
 package com.facturas.Facturas_Electronicas.Admins.service;
 
+import com.facturas.Facturas_Electronicas.Admins.repository.AdminRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
+    private final AdminRepository adminRepository;
+    public AdminService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
 }
