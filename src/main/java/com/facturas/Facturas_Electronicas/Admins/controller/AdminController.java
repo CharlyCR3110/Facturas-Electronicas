b68b985653dff601a6125c36ba5a73ca72351cc0
Charly <carlosgarita3110@gmail.com>
2024-03-29 11:46:45 -0600

    feat: add AdminService dependency injection and HttpSession in AdminController constructor

--- src/main/java/com/facturas/Facturas_Electronicas/Admins/controller/AdminController.java
+++ src/main/java/com/facturas/Facturas_Electronicas/Admins/controller/AdminController.java
@@ -1,10 +1,20 @@
 package com.facturas.Facturas_Electronicas.Admins.controller;
 
+import com.facturas.Facturas_Electronicas.Admins.service.AdminService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes({"currentProvider", "currentProviderList"})
 public class AdminController {
+    private final AdminService adminService;
 
+    @Autowired
+    HttpSession httpSession;
+    
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 }
