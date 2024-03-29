43ed6faf436891c156ebc094ba1841200594b79d
Charly <carlosgarita3110@gmail.com>
2024-03-29 11:44:41 -0600

    feat: add AdminController class

---
+++ src/main/java/com/facturas/Facturas_Electronicas/Admins/controller/AdminController.java
@@ -0,0 +1,10 @@
+package com.facturas.Facturas_Electronicas.Admins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes({"currentProvider", "currentProviderList"})
+public class AdminController {
+
+}
