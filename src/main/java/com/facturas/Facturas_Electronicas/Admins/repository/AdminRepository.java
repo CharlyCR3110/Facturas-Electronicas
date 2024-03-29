8df3e7f4e4ea02141ae70e99a455a08931503d4e
Charly <carlosgarita3110@gmail.com>
2024-03-29 11:43:26 -0600

    feat: add AdminRepository interface

---
+++ src/main/java/com/facturas/Facturas_Electronicas/Admins/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package com.facturas.Facturas_Electronicas.Admins.repository;
+
+import com.facturas.Facturas_Electronicas.Admins.model.AdminEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<AdminEntity, Integer> {
+
+}
